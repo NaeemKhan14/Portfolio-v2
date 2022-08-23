927ec1c773f60f272f2c48e40f5a6184e5443f22
Naeem Khan <n.khan@archirodon.net>
2022-08-23 18:03:02 +0400

    Nav, footer and hero content added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,47 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        "./src/**/*.{html,ts}",
+        "./src/**/*.{html,ts,scss,css}",
+        "./node_modules/flowbite/**/*.js"
     ],
+    safelist: [
+        'w-64',
+        'w-1/2',
+        'rounded-l-lg',
+        'rounded-r-lg',
+        'bg-gray-200',
+        'grid-cols-4',
+        'grid-cols-7',
+        'h-6',
+        'leading-6',
+        'h-9',
+        'leading-9',
+        'shadow-lg'
+    ],
+    darkMode: 'class',
     theme: {
+        colors: {
+            bgColor: '#353353',
+            btnBg: '#ff4c60',
+            accentColor: '#ffd15c',
+        },
+        container: {
+            center: true,
+            padding: {
+                DEFAULT: '1rem',
+                sm: '2rem',
+                lg: '2rem',
+                xl: '2rem',
+                '2xl': '2rem',
+            },
+        },
+        fontFamily: {
+            sans: ['Rubik', 'sans-serif'],
+            serif: ['Libre Baskerville', 'serif'],
+        },
         extend: {},
     },
-    plugins: [],
+    plugins: [
+        require('flowbite/plugin')
+    ],
 }
