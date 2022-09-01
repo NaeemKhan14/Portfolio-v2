51fabfbd2d975b3efd66adebd3816784b510adcc
Naeem Khan <n.khan@archirodon.net>
2022-09-01 11:27:43 +0400

    Changed breakpoints and optimized navbar links

--- src/app/core/layout/navbar/navbar.component.ts
+++ src/app/core/layout/navbar/navbar.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+    navLinks: any[] = [];
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.navLinks = [
+            { name: 'Home', url: '#' },
+            { name: 'About', url: '#' },
+            { name: 'Projects', url: '#' },
+            { name: 'Contact', url: '#' },
+        ]
+    }
 
 }
