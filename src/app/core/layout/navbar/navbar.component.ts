3b56805b4dc8e9065dcc14951816a6a1779d69ea
Naeem Khan <n.khan@archirodon.net>
2022-11-07 11:08:31 +0400

    Finished navigation

--- src/app/core/layout/navbar/navbar.component.ts
+++ src/app/core/layout/navbar/navbar.component.ts
@@ -1,23 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-    selector: 'app-navbar',
-    templateUrl: './navbar.component.html',
-    styleUrls: ['./navbar.component.scss']
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-    navLinks: any[] = [];
+  navLinks: any[] = [];
 
-    constructor() { }
+  constructor() {}
 
-    ngOnInit(): void {
-        this.navLinks = [
-            { name: 'Home', url: '#' },
-            { name: 'About', url: '#' },
-            { name: 'Experience', url: '#' },
-            { name: 'Projects', url: '#' },
-            { name: 'Contact', url: '#' },
-        ]
-    }
+  ngOnInit(): void {
+    this.navLinks = [
+      { name: 'Home', url: 'home' },
+      { name: 'About', url: 'about' },
+      { name: 'Experience', url: 'experience' },
+      { name: 'Projects', url: 'projects' },
+      { name: 'Contact', url: 'contact' },
+    ];
+  }
 
+  scroll(el: any) {
+    document.getElementById(el)?.scrollIntoView({behavior: 'smooth'});
+  }
 }
