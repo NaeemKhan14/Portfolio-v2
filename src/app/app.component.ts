7dafe0ec386b238e49be9d224cb8b5fcfd51d849
Naeem Khan <n.khan@archirodon.net>
2022-11-07 10:37:35 +0400

    Added gototop btn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Naeem Khan';
+  isVisible = false;
+  gotoTopPosVisible = 100;
+
+  @HostListener('window:scroll')
+  checkScroll() {
+    // Both window.pageYOffset and document.documentElement.scrollTop returns the same result in all the cases. 
+    // window.pageYOffset is not supported below IE 9.
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    
+    this.isVisible = true ? scrollPosition >= this.gotoTopPosVisible : false;
+  }
+
+  gotoTop() {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
+  }
+
 }
